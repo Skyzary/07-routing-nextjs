038c3318bf9fa95c0d3ae693648d29339f6233a0
Volodymyr Levkovskyi <132926053+Skyzary@users.noreply.github.com>
2026-03-13 16:01:05 +0100

    Update next.config.ts to ignore ESLint during builds

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  /* config options here */
+  experimental: {
+    // missingSuspenseWithCSRBailout: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
 };
 
 export default nextConfig;
